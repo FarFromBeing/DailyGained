2f886c1b9228551c7e1aea0109f4f25820b80830
FarFromBeing <31037739+FarFromBeing@users.noreply.github.com>
2018-10-01 20:49:39 +0800

    op

---
+++ 20180928-TensorFlow/初识神经网络/例子/Session控制.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+'''
+Session 是 Tensorflow 为了控制,和输出文件的执行的语句
+'''
+mat1=tf.constant([[3,3]])#矩阵
+mat2=tf.constant([[3],[2]])
+
+product=tf.matmul(mat1,mat2)#两个矩阵相乘
+'''
+product并无法执行，只能通过session的一个对象来run，才能运行
+2种运行tf.Session()方式
+'''
+#1.
+# with tf.Session() as sess:
+#     result=sess.run(product)
+#     print(result)
+# #[[15]]
+
+#2.
+sess=tf.Session()
+res=sess.run(product)
+print(res)
+sess.close()
